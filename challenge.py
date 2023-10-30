7ce064ef9875a7c0f0c799d583a531e44371976b
SHARV1L <livrahs@hotmail.com>
2023-10-30 01:35:51 -0400

    Level 1 and 2 completed with comments

--- challenge.py
+++ challenge.py
@@ -3,12 +3,16 @@ class learningBot:
     def __init__(self):
         self.levels = [
             {
-                "sample": "print('Hello World')",
+                # introducing typo error in print
+                "level1": "print('Hello World')",
                 "error1": "prit('Hello World')"
              },
             {
-                "sample": "print",
-                "error2": ""
+                # introducing semantic error by printing: print(10); and syntax error by ignoring "."
+                "level2": "for i in range(3): "
+                          "print(i)",
+                "error2": "for i in range(3)"   
+                          "print(10)"
             }
         ]
 
