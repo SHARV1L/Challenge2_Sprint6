52d4fdde6924c77917c62e98a38388cb14664763
SHARV1L <livrahs@hotmail.com>
2023-10-30 01:58:22 -0400

    Lean main class which calls bot class in main function

--- challenge.py
+++ challenge.py
@@ -1,19 +1,25 @@
-class learningBot:
+class programLearningBot:
 
     def __init__(self):
         self.levels = [
+            # level 1: introducing typo error in print
             {
-                # introducing typo error in print
-                "level1": "print('Hello World')",
-                "error1": "prit('Hello World')"
+                "level": "print('Hello World')",
+                "error": "prit('Hello World')"
              },
+            # level 2: introducing semantic error by printing: print(10); and syntax error by ignoring ":"
             {
-                # introducing semantic error by printing: print(10); and syntax error by ignoring "."
-                "level2": "for i in range(3): "
+                "level": "for i in range(3): "
                           "print(i)",
-                "error2": "for i in range(3)"   
+                "error": "for i in range(3)"   
                           "print(10)"
-            }
+            },
+            # level 3: introducing syntax error in addition/subtraction
+            # {
+            #     print(Check out for
+            #     "level": ""
+            # }
+
         ]
 
         self.currentLevel = 0
--- main.py
+++ main.py
@@ -1,46 +1,9 @@
-class ProgrammingLearningBot:
 
-    def __init__(self):
-        self.levels = [
-            {"task": "print('Hello World')", "error": "prit('Hello World')"},
-            {"task": "for i in range(3): print(i)", "error": "for i in range(3) print(i)"},
-            {"task": "def add(a, b): return a + b", "error": "def add(a, b) return a + b"},
-            {"task": "if x > 10: print('Big')", "error": "if x > 10 print('Big')"},
-            {"task": "data = {'key': 'value'}; print(data['key'])", "error": "data = {'key': 'value'}; print(data[key])"},
-            {"task": "lst = [1,2,3]; print(lst[2])", "error": "lst = [1,2,3]; print(lst[3])"},
-            {"task": "while x < 5: x += 1", "error": "while x < 5 x += 1"},
-            {"task": "import math; print(math.sqrt(16))", "error": "import math; print(math.sqroot(16))"},
-            {"task": "def greet(): return 'Hello'", "error": "def greet(): return Hello"},
-            {"task": "for i in [1,2,3]: print(i)", "error": "for i in (1,2,3): print(i)"}
-        ]
-        self.current_level = 0
+from challenge import programLearningBot
 
-    def get_current_challenge(self):
-        if self.current_level < len(self.levels):
-            return self.levels[self.current_level]['error']
-        return "Congratulations! You've completed all the challenges."
+def main():
+    bot = programLearningBot()
+    print(bot.levels[bot.currentLevel])
 
-    def submit_solution(self, solution):
-        correct_solution = self.levels[self.current_level]['task']
-        if solution == correct_solution:
-            self.current_level += 1
-            return "Correct! Moving to next challenge."
-        else:
-            return "Incorrect. Try again!"
-
-    def get_solution_hint(self):
-        return self.levels[self.current_level]['task']
-
-# Usage:
-
-bot = ProgrammingLearningBot()
-print("Welcome to the Programming Learning Bot!")
-print("Here's your first challenge:")
-
-while True:
-    print("\nChallenge:", bot.get_current_challenge())
-    solution = input("Enter your solution: ")
-    feedback = bot.submit_solution(solution)
-    print(feedback)
-    if feedback == "Congratulations! You've completed all the challenges.":
-        break
+if __name__ == "__main__":
+    main()
